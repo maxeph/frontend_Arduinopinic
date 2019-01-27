da15b7ebb22b6e67087e3d300602f1f436a74d19
Stephane M <s.manguette@gmail.com>
2019-01-27 23:33:47 +0100

    Implementing Bootstrap 4 framework
    adding js/css static file
    changing skeleton of index.html

--- Arduinopinic/views.py
+++ Arduinopinic/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-version_global ='0.01'
+version_global ='0.01a'
 def index(request):
 	version = version_global
 	return render(request, 'index.html', locals())
