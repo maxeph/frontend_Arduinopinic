fe59f2e66a3ab86ee01f666e0a0fd995d00c376e
Stephane M <s.manguette@gmail.com>
2019-01-28 23:15:26 +0100

    Implementing Ajax request to update widgets
    1) 4 variables retrieved :
    - temp
    - humidity
    - water temp
    - status of daemon
    
    2) Implementing view in django
    3) JS script - updating frontend accordingly + error handling

--- Arduinopinic/models.py
+++ Arduinopinic/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class temp_db(models.Model):
+	date = models.DateTimeField()
+	tempext = models.DecimalField(max_digits=4, decimal_places=2)
+	tempeau = models.DecimalField(max_digits=4, decimal_places=2)
+	humid = models.DecimalField(max_digits=4, decimal_places=2)
+
+	def __str__(self):
+		return str(self.date)
--- Arduinopinic/static/js/basics.js
+++ Arduinopinic/static/js/basics.js
@@ -1,4 +1,42 @@
 function pop_alert(msg, type)
 {
-$('#alert_box').append('<div class=\"alert alert-' + type + '\" role=\"alert\">' + msg + '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>');
+  $('#alert_box').append('<div class=\"alert alert-' + type + '\" role=\"alert\">' + msg + '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>');
 };
+function updateWidgets()
+{
+  $.getJSON( updateURL, function(data) {
+    $('#vtemp').text(Number(data.tempext).toLocaleString('fr-fr'));
+    $('#vhumid').text(Number(data.humid).toLocaleString('fr-fr'));
+    $('#veau').text(Number(data.tempeau).toLocaleString('fr-fr'));
+    $('#wtemp').css('border-color', '#428bca');
+    $('#whumid').css('border-color', '#428bca');
+    $('#weau').css('border-color', '#428bca');
+    $('#wtemp').css('color', '#428bca');
+    $('#whumid').css('color', '#428bca');
+    $('#weau').css('color', '#428bca');
+    if (data.status) {
+      $('#vstatus').css('color', 'green');
+    }
+    else {
+      $('#vstatus').css('color', 'red');
+      pop_alert("<Strong>Alert !</Strong> The Daemon is not up. Make sure to launch the Daemon","danger")
+      /*
+      #######Sure there is more to implement here!!!!!!
+      */
+    }
+    setTimeout(updateWidgets, 5000);
+  })
+  .fail(function(data) {
+    pop_alert("<Strong>Alert !</Strong> The Server is not reachable. Make sure that the Pi or Django is correctly running. Then reload.","danger")
+    $('#wtemp').css('border-color', 'red');
+    $('#whumid').css('border-color', 'red');
+    $('#weau').css('border-color', 'red');
+    $('#wtemp').css('color', 'red');
+    $('#whumid').css('color', 'red');
+    $('#weau').css('color', 'red');
+    $('#wlevel').css('color', 'red');
+    $('#wlevel').css('border-color', 'red');
+    $('#vstatus').css('color', 'red');
+  });
+}
+updateWidgets();
