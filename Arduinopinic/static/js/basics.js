ddf3b74c5f2d15383b5b3211c66b4665d88610b2
Stephane M <s.manguette@gmail.com>
2019-01-28 01:06:18 +0100

    Adding alert box function
    Implementing container for alert
    Small css definition
    JS function to allow easy pop up

---
+++ Arduinopinic/static/js/basics.js
@@ -0,0 +1,4 @@
+function pop_alert(msg, type)
+{
+$('#alert_box').append('<div class=\"alert alert-' + type + '\" role=\"alert\">' + msg + '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>');
+};
