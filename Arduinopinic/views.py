58598177e674a78f035a5cc02c91a704bf60b99e
Stephane M <s.manguette@gmail.com>
2019-01-31 08:49:03 +0100

    Update of JS widgetupdate func and related djangoview
    Views.py
    - updateWidgets is now only sending last entry in table. the  24h values will be sent via other ajax request since the latter is heavier but only called once, so no need to seek for these value every x seconds
    - test if table is empty and if yes sending info to JS function
    basics.js
    - the update of css (color or text) is now in separate function for easier call
    - check if table was not empty
    - date is now changed in JS date format
    - check if value is not outdated meaning issues with daemon (with moment.js)
    - popup now only happen once (not every x seconds)

--- Arduinopinic/views.py
+++ Arduinopinic/views.py
@@ -13,18 +13,22 @@ def index(request):
 	return render(request, 'index.html', locals())
 
 def updateWidgets(request):
-	server = dict()
-	end_range = datetime.now()
-	range = timedelta(days=1)
-	beg_range = end_range - range
-	chart = temp_db.objects.filter(date__gte=beg_range)
-	last_entry = chart.latest('date')
-	if	(len(os.popen("pgrep -f piserver.py").readlines())) > 1:  # piserver is running
+	server = dict() # dict to be sent via Json
+	###### Test if DB populated and taking last entry######################
+	if temp_db.objects.last():
+		last_entry = temp_db.objects.last()
+		server["isnotempty"] = True
+		server["date"] = last_entry.date
+		server["tempext"] = last_entry.tempext
+		server["tempeau"] = last_entry.tempeau
+		server["humid"] = last_entry.humid
+	else:
+		server["isempty"] = False
+	#########################################################################
+	###### Test if daemon is running ########################################
+	if	(len(os.popen("pgrep -f piserver.py").readlines())) > 1:
 		server["status"] = True
-	else:							# piserver is not running
+	else:
 		server["status"] = False
-	server["date"] = last_entry.date
-	server["tempext"] = last_entry.tempext
-	server["tempeau"] = last_entry.tempeau
-	server["humid"] = last_entry.humid
+	#########################################################################
 	return JsonResponse(server)
