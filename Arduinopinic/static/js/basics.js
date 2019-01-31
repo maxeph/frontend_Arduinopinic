30baacc3457a664fb7260aeb5ad51179c7739d9a
Stephane M <s.manguette@gmail.com>
2019-01-31 08:49:03 +0100

    Update of JS widgetupdate func and related djangoview
    Views.py
    - updateWidgets is now only sending last entry in table. the  24h values will be sent via other ajax request since the latter is heavier but only called once, so no need to seek for these value every x seconds
    - test if table is empty and if yes sending info to JS function
    basics.js
    - the update of css (color or text) is now in separate function for easier call
    - check if table was not empty
    - date is now changed in JS date format
    - check if value is not outdated meaning issues with daemon (with moment.js)
    - popup now only happen once (not every x seconds)

--- Arduinopinic/static/js/basics.js
+++ Arduinopinic/static/js/basics.js
@@ -1,42 +1,71 @@
+var daemonNotWorking = false;
+var noDataDB = false;
+var outdated = false;
+
+/* Function to handle alerts in the correspondig div*/
 function pop_alert(msg, type)
 {
   $('#alert_box').append('<div class=\"alert alert-' + type + '\" role=\"alert\">' + msg + '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>');
 };
+/* Function change color of widgets*/
+function colorWidgets(color)
+{
+  $('#wtemp').css('border-color', color);
+  $('#whumid').css('border-color', color);
+  $('#weau').css('border-color', color);
+  $('#wtemp').css('color', color);
+  $('#whumid').css('color', color);
+  $('#weau').css('color', color);
+  $('#wlevel').css('color', color);
+  $('#wlevel').css('border-color', color);
+  $('#vstatus').css('color', color);
+}
+/* Function to update values via css*/
+function valuesWidgets(pack,locales)
+{
+  $('#vtemp').text(Number(pack.tempext).toLocaleString(locales));
+  $('#vhumid').text(Number(pack.humid).toLocaleString(locales));
+  $('#veau').text(Number(pack.tempeau).toLocaleString(locales));
+}
+/* Function get metrics via Ajax and update widgets*/
 function updateWidgets()
 {
   $.getJSON( updateURL, function(data) {
-    $('#vtemp').text(Number(data.tempext).toLocaleString('fr-fr'));
-    $('#vhumid').text(Number(data.humid).toLocaleString('fr-fr'));
-    $('#veau').text(Number(data.tempeau).toLocaleString('fr-fr'));
-    $('#wtemp').css('border-color', '#428bca');
-    $('#whumid').css('border-color', '#428bca');
-    $('#weau').css('border-color', '#428bca');
-    $('#wtemp').css('color', '#428bca');
-    $('#whumid').css('color', '#428bca');
-    $('#weau').css('color', '#428bca');
+    if (data.isnotempty) {
+      data.date = new Date(data.date); /* Coding string date in JS date */
+      valuesWidgets(data,'fr-fr')
+      if (moment.duration(moment(data.date).diff()) < (-300000)) { /* if values older than 5 minutes*/
+        if (outdated === false) {
+          pop_alert("<Strong>Alert !</Strong> It seems that the Daemon has not been running for " + moment.duration(moment(data.date).diff()).humanize() + ". An outdated value is disclosed.","warning");
+          outdated = true;
+        }
+      }
+      else {
+        colorWidgets('#428bca');
+      }
+    }
+    else {
+      if (noDataDB === false) {
+        pop_alert("<Strong>Error !</Strong> No data in the database. Make sure to launch the Daemon first.","warning");
+        noDataDB = true;
+      }
+    }
+
     if (data.status) {
       $('#vstatus').css('color', 'green');
     }
     else {
-      $('#vstatus').css('color', 'red');
-      pop_alert("<Strong>Alert !</Strong> The Daemon is not up. Make sure to launch the Daemon","danger")
-      /*
-      #######Sure there is more to implement here!!!!!!
-      */
+      if (daemonNotWorking === false) {
+        $('#vstatus').css('color', 'red');
+        pop_alert("<Strong>Error !</Strong> The Daemon is not running. Make sure to launch the Daemon.","danger");
+        daemonNotWorking = true;
+      }
     }
     setTimeout(updateWidgets, 5000);
   })
   .fail(function(data) {
-    pop_alert("<Strong>Alert !</Strong> The Server is not reachable. Make sure that the Pi or Django is correctly running. Then reload.","danger")
-    $('#wtemp').css('border-color', 'red');
-    $('#whumid').css('border-color', 'red');
-    $('#weau').css('border-color', 'red');
-    $('#wtemp').css('color', 'red');
-    $('#whumid').css('color', 'red');
-    $('#weau').css('color', 'red');
-    $('#wlevel').css('color', 'red');
-    $('#wlevel').css('border-color', 'red');
-    $('#vstatus').css('color', 'red');
+    pop_alert("<Strong>Error !</Strong> The Server is not reachable. Make sure that the Pi or Django is correctly running. Then reload.","danger");
+    colorWidgets('red');
   });
 }
 updateWidgets();
